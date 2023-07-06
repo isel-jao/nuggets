d14a3c6b6d798ffdb3f4995ab125487cbc460726
isel-jao <iseljao89@gmail.com>
2023-07-06 10:18:44 +0100

    fresh start

--- trpc/server/common/redis.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-
-const redis = createClient();
-
-redis.on("error", (err) => console.log("Redis Client Error", err));
-
-redis.connect();
-
-export default redis;
