ad3f6acbfe9de01f93da37f078f1ea2dfb973c35
issam el jaouhary <iseljao89@gmail.com>
2025-04-29 10:42:29 +0100

    Add react-scan integration for development environment

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import Router from "./pages/router/index.tsx";
+import { scan } from "react-scan";
+import { config } from "./config/index.ts";
+
+scan({
+  enabled: config.environment === "development",
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router />
-  </StrictMode>
+  </StrictMode>,
 );
