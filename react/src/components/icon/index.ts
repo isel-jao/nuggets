75e6e9ac3d8ac2a79ed677babd0b2c63b6e140f8
issam el jaouhary <iseljao89@gmail.com>
2026-08-06 16:05:46 +0100

    react: initial vite-module-federation

--- react/src/components/icon/index.ts
+++
@@ -1,2 +0,0 @@
-export { Icon, type IconProps } from "./icon";
-export { registry } from "./registry";
--- react/src/components/icon/types.ts
+++
@@ -1,5 +0,0 @@
-import React, { type ComponentType, type SVGProps } from "react";
-
-export type IconComponent = React.LazyExoticComponent<
-  ComponentType<SVGProps<SVGSVGElement>>
->;
